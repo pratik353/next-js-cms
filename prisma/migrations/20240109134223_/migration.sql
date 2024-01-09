9e649dd1de8e816bd1ac671fdde87e6ee539a3ab
megan fernandes <megan.fernandes@infogen-labs.com>
2024-01-09 19:29:04 +0530

    function to add new blog

--- prisma/migrations/20240109134223_/migration.sql
+++ prisma/migrations/20240109134223_/migration.sql
@@ -15,9 +15,8 @@ CREATE TABLE "blog" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(2048) NOT NULL,
     "slug" VARCHAR(2048) NOT NULL,
-    "status" VARCHAR(255) NOT NULL,
+    "status" VARCHAR(255) NOT NULL DEFAULT 'active',
     "flags" INTEGER NOT NULL,
-    "comments" VARCHAR(2048) NOT NULL DEFAULT 'none',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "userId" INTEGER NOT NULL,
@@ -39,8 +38,17 @@ CREATE TABLE "tags" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(200) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "catId" INTEGER NOT NULL,
 
     CONSTRAINT "tags_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" SERIAL NOT NULL,
+    "cat_name" VARCHAR(200) NOT NULL DEFAULT 'other',
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -63,19 +71,25 @@ CREATE TABLE "_blogTotags" (
 CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "comments_blogId_key" ON "comments"("blogId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_blogTotags_AB_unique" ON "_blogTotags"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_blogTotags_B_index" ON "_blogTotags"("B");
 
 -- AddForeignKey
-ALTER TABLE "blog" ADD CONSTRAINT "blog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "blog" ADD CONSTRAINT "blog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "blog"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "comments" ADD CONSTRAINT "comments_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "blog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Block" ADD CONSTRAINT "Block_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "blog"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "tags" ADD CONSTRAINT "tags_catId_fkey" FOREIGN KEY ("catId") REFERENCES "category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Block" ADD CONSTRAINT "Block_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "blog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_blogTotags" ADD CONSTRAINT "_blogTotags_A_fkey" FOREIGN KEY ("A") REFERENCES "blog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/actions/add-new-blog.ts
+++ src/actions/add-new-blog.ts
@@ -1,17 +1,58 @@
 "use server";
+import prisma from "@/app/prismadb";
 
 interface BlogFormData {
-    tags: String[];
-    blog: any
+  tags: String[];
+  blog: any;
 }
 
-export const addNewBlog = (formData: BlogFormData) => {
-    console.log("POST: blogs/add-new");
-    console.log(formData);
+export const addNewBlog = async (formData: BlogFormData) => {
+  console.log("POST: blogs/add-new");
+  console.log(formData);
 
-    const tags = formData.tags
-    const allBlocks = formData.blog["blocks"]
+  const tags = formData.tags;
+  const allBlocks = formData.blog["blocks"];
+  let getTagsId: Array<number> = [];
 
-    console.log("XXXXXXXXXXXXXX",tags,allBlocks)
-    
-}
+  const promises = tags.map(async (item: any) => {
+    // console.log(item)
+    let tagId = await prisma.tags.findFirst({
+      where: {
+        name: item,
+      },
+    });
+    console.log(tagId);
+    getTagsId.push(tagId!.id);
+  });
+
+  await Promise.all(promises);
+
+  console.log("XXXXXXXXXXXXXX", tags, allBlocks);
+
+  const blog = await prisma.blog.create({
+    data: {
+      title: "hello dummy",
+      slug: "hello-dummy",
+      status: "active",
+      flags: 1,
+      userId: 1,
+      tags: {
+        connect: getTagsId.map((id) => ({
+          id,
+        })),
+      },
+    },
+  });
+
+  allBlocks.forEach(async function (item: any, index: number) {
+    console.log(item);
+
+    const addBlock = await prisma.block.create({
+      data: {
+        blogId: blog.id,
+        sequence: index + 1,
+        blockData: JSON.stringify(item),
+      },
+    });
+  });
+};
